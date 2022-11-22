1b5118197c698330b8817123b24991433a39611d
SanskarGosavi2003 <210100132@iitb.ac.in>
2022-11-22 19:23:53 +0530

    first commit

---
+++ contracts/ToldSo.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+
+contract ToldSo{
+    struct Post{
+        string title;
+        string content;
+        address author;
+        uint256 timestamp;
+    }
+
+    mapping(address => Post[]) private _authorToPosts
+
+    function createPost(string memory title, string memory body) external {
+        Post memory post = Post(title, body, block.timestamp);
+        authorToPosts[msg.sender].push(post);
+    }
+
+    function getPostsByAuthor(address author)
+        external
+        view
+        returns (Post[] memory)
+    {
+        return authorToPosts[author];
+    }
+}
